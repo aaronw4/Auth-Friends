72c49b8d83b077a57d72e7cb1320e6630ac336e2
Aaron Williams <aaronrw4@yahoo.com>
2019-12-06 19:55:50 -0800

    Created Routes

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import Login from './Components/Login';
+import Friends from './Components/Friends';
+import PrivateRoute from './Components/PrivateRoute';
+import {BrowserRouter, Route} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Route path='/login' component={Login} />
+        <PrivateRoute exact path='/protected' component={Friends} />
+        <Route exact path='/' component={Login} />
+      </div>
+    </BrowserRouter>
   );
 }
 
